5f62fc2c11adca999cbee0ef6a55f9c3d3791869
Victor M. Ortiz <vicortiz1993@gmail.com>
2022-07-19 23:51:15 -0400

    update posts
    Changed the post create view forms blog name with the display name annotation and made the value thats being shown be the blog title name rather than the description in the posts controller.

--- Models/Post.cs
+++ Models/Post.cs
@@ -13,7 +13,10 @@ namespace TheBlogProject.Models
     public class Post
     {
         public int Id { get; set; }
+
+        [Display(Name = "Blog Name")]
         public int BlogId { get; set; }
+
         public string BlogUserId { get; set; }
 
         [Required]
