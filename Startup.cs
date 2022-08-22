9119ac929090cb9add599c135059a1f6e62ae4f4
Victor M. Ortiz <vicortiz1993@gmail.com>
2022-08-21 21:52:36 -0400

    make slug service
    make a slug service interface and a class that will be implementing that interface

--- Startup.cs
+++ Startup.cs
@@ -55,6 +55,9 @@ namespace TheBlogProject
 
             //Register images Service//
             services.AddScoped<IImageService, BasicImageService>();
+
+            //Register the Slug Service//
+            services.AddScoped<ISlugService, BasicSlugService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
